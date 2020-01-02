552cdba75c5ebedc42dd838e87035e952e29933d
pratiksh404 <pratikdai404@gmail.com>
2020-01-02 17:40:13 +0545

    Initial Commit

---
+++ literal_and_string_interpolation.dart
@@ -0,0 +1,27 @@
+void main(){
+  /* Literals */
+  var defBool = true;
+  var number = 23;
+  int n1 = 2;
+  int n2 = 3;
+
+    String s1 = '2';
+  String s2 = '3';
+  "drh2so4";
+  /* Assigning Literals to string or define string literals */
+  String var1 = 'batman';
+  String var2 = 'joker';
+  String var3 = 'I\'m Batman';
+  String var4 = "I'm Batman"; /* See the basic difference */
+
+/*  String Interpolation */
+print("I'm $var1");
+print("the number of character in var1 is ${var1.length}");
+print("sum of n1 and n2 is ${n1 + n2}");
+print("sum of s1 and s2 is ${s1 + s2}");
+  /* print section */
+  print(var1);
+    print(var2);
+      print(var3);
+        print(var4);
+}
